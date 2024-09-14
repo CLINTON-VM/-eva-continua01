73cf476a5f2e39ed73261d43946727ea2ddf4f60
CLINTON-VM <74558654@idexperujapon.edu.pe>
2024-09-14 17:07:03 -0500

    mi primer comit

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ $img_sliders = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>evaluacion02</title>
-    <link rel="stylesheet" href="vendor/css/bootstrap.min.css">
+    <link rel="stylesheet" href="resources/css/bootstrap.min.css">
 </head>
 
 <body>
